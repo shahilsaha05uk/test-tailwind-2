365d9021563fda70b24cb0deae394e552cf36ea9
Shahil Saha <s015695k@student.staffs.ac.uk>
2024-12-17 10:23:37 +0000

    c

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function App() {
 	return (
 		<View>
-			<Text className={"text-9xl text-red-600"}>Hello World from Tailwind</Text>
+			<Text className={"text-9xl text-red-900"}>Hello World from Tailwind</Text>
 		</View>
 	);
 }
